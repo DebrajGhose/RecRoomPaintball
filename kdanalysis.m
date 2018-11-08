94dcd22f75ec1ac8d42cf5df638d74a626a3be85
DebrajGhose <dg.chaos06@gmail.com>
2018-11-08 02:19:43 -0500

    kd ratios and graphs

---
+++ kdanalysis.m
@@ -0,0 +1,59 @@
+% See how players performed during playoffs
+clear all
+close all
+load('Season3playoffdata.mat');
+
+kd = [];
+maxkd = [];
+
+for h = 1:numel(Players)
+    
+    name = Players{h};
+    
+    playerkd = [];
+    currmax = 0;
+    
+    for i = 2:numel(Season3Playoffs(:,1))
+        
+        if strcmp(name,char(Season3Playoffs(i,4)))
+           
+            playerkd = [playerkd , Season3Playoffs{i,7}/Season3Playoffs{i,8} ];
+            currmax = max(currmax , Season3Playoffs{i,7}/Season3Playoffs{i,8}  );
+        end
+        
+    end
+    
+    kd = [ kd ; mean(playerkd)];
+    maxkd = [maxkd ; currmax];
+    
+    hold on
+    scatter(h*ones(1,numel(playerkd)),playerkd ,'fill');
+    
+end
+
+xticks([1:numel(Players)])
+xticklabels(Players);
+xtickangle(60)
+
+%arrange everything
+
+Playerdata = {};
+
+for i = 1:numel(Players)
+   
+    Playerdata{i,1} = Players{i};
+    Playerdata{i,2} = kd(i);
+    Playerdata{i,3} = maxkd(i);
+    
+end
+
+% Find player highest kd
+
+
+
+
+
+
+
+
+
