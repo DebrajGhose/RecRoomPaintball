922c33fa1445447f0c38bed99e437eaaa43a422b
DebrajGhose <dg.chaos06@gmail.com>
2018-11-26 16:15:07 -0500

    Better colors

--- KDAnalysis_Playoffs.m
+++ KDAnalysis_Playoffs.m
@@ -4,7 +4,6 @@ close all
 load('Season3playoffdata.mat');
 load('Players.mat');
 
-
 %spread of all player kds
 
 kd = [];
@@ -46,19 +45,32 @@ figure
 
 for i = 2:size(Players,1)
     hold on
-   scatter(  i*ones(1,numel(cell2mat(Players(i,4)))),cell2mat(Players(i,4)),'fill');
+    
+   dotcolor = choosedotcolor(char(Players(i,3)));
+   scatter(  i*ones(1,numel(cell2mat(Players(i,4)))),cell2mat(Players(i,4)),'MarkerFaceColor',dotcolor,'MarkerEdgeColor','none');
     
 end
 
 xticks(2:size(Players,1))
 xticklabels(Players(2:end,1));
+set(gca,'TickLabelInterpreter','none');
 xtickangle(60)
 
+% generate color matrix
+
+colordots = [];
+
+for i = 2:size(Players,1)
+   
+    dotcolor = choosedotcolor(char(Players(i,3)));
+    colordots = [ colordots ; dotcolor ];
+    
+end
 
 %cost vs kd graph
 
 figure
-plot(cell2mat(Players(2:end,2)),cell2mat(Players(2:end,5)),'.');
+scatter(cell2mat(Players(2:end,2)),cell2mat(Players(2:end,5)),30,colordots,'fill');
 p = polyfit(cell2mat(Players(2:end,2)),cell2mat(Players(2:end,5)),1);
 hold on
 plot( cell2mat(Players(2:end,2)) , p(1)*cell2mat(Players(2:end,2)) + p(2));
@@ -67,9 +79,36 @@ xlabel('Player Cost'); ylabel('Avg kd');
 
 figure
 %cost vs max kd
-plot(cell2mat(Players(2:end,2)),cell2mat(Players(2:end,6)),'.');
+scatter(cell2mat(Players(2:end,2)),cell2mat(Players(2:end,6)),30,colordots,'fill');
 p = polyfit(cell2mat(Players(2:end,2)),cell2mat(Players(2:end,6)),1);
 hold on
 plot( cell2mat(Players(2:end,2)) , p(1)*cell2mat(Players(2:end,2)) + p(2));
 
 xlabel('Player Cost'); ylabel('Max kd');
+
+
+function [dotcolor] = choosedotcolor(teamname)
+
+
+switch teamname
+    
+    case 'Black'
+        dotcolor = [ 0 0 0 ];
+    case 'Orange'
+        dotcolor = [ 1 0.6 0.2 ];
+    case 'Teal'
+        dotcolor = [ 0.7 1 1 ];
+    case 'Purple'
+        dotcolor = [ 0.8 0.4 0.8 ];
+    case 'Pink'
+        dotcolor = [ 1 0.5 0.5 ];
+    case 'Yellow'
+        dotcolor = [ 0.9 0.9 0.4 ];
+    case 'Green'
+        dotcolor = [ 0.5 1 0.5 ];
+    case 'White'
+        dotcolor = [ 0.5 0.5 0.5 ];
+        
+end
+
+end
